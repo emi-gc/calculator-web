6cd5c31f0f83180d71966ef0a0ec2aa2ba135196
APaoOB <0271535@up.edu.mx>
2026-09-10 16:46:41 -0600

    Changes in Javascript to make the calculator useful

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+const theme = document.getElementById("theme");
+
 let num1 = null;
 let operation = null;
 let applyReset = false;
@@ -35,8 +37,65 @@ function calculator(numA, numB, callback) {
     return callback(numA, numB);
 }
 
-const theme = document.getElementById("theme");
+const keys = document.querySelectorAll('.key');
 
+function showDisplay(value) {
+    resultStatus.textContent = value;
+}
+
+keys.forEach((key) => {
+    key.addEventListener('click', () => {
+        const num = key.dataset.number;
+        const op = key.dataset.operator;
+        const action = key.dataset.action;
+
+        if (num === "delete") {
+            showDisplay(resultStatus.textContent.slice(0, -1));
+            return;
+        }
+
+        if (action === "reset") {
+            num1 = null;
+            operation = null;
+            applyReset = false;
+            showDisplay("0");
+            return;
+
+        }
+
+        if (action === "decimal") {
+            if (!resultStatus.textContent.includes(".")) {
+                showDisplay(resultStatus.textContent + ".");
+            }
+            return;
+        }
+
+        if(op) {
+            num1 = Number(resultStatus.textContent);
+            operation = op;
+            applyReset = true;
+            return;
+        }
+
+        if (action === "equals" && operation) {
+            const result1 = calculator(num1, Number(resultStatus.textContent), Jerarquia[operation].callback);
+            showDisplay(result1 instanceof Error ? result1.message : result1);
+            num1 = null;
+            operation = null;
+            applyReset = false;
+        }
+
+        if (num !== undefined) {
+            if (applyReset || resultStatus.textContent === "Error") {
+                showDisplay(num);
+                applyReset = false;
+            } else {
+                showDisplay(resultStatus.textContent === "0" ? num : resultStatus.textContent + num);
+            }
+            return;
+        }
+    });
+});
 const currentTheme = ["theme-1", "theme-2", "theme-3"].findIndex((className) =>
     document.body.classList.contains(className)
 );
@@ -46,3 +105,4 @@ theme.addEventListener("input", function() {
     document.body.classList.remove("theme-1","theme-2", "theme-3");
     document.body.classList.add("theme-" + (Number(theme.value) + 1));
 });
+
